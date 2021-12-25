83f8d743d43d7ac653ee1fc30020c3b74af98ad0
amami0522 <31738@toyota.kosen-ac.jp>
2021-12-25 23:37:50 +0900

    カテゴリー一覧の追加

---
+++ components/Cards.vue
@@ -0,0 +1,22 @@
+<template>
+    <v-card max-width="600">
+        <v-img
+            :src="image"
+            class="white--text align-end"
+            height="300px"
+        >
+            <v-card-title>
+                {{ category }}
+            </v-card-title>
+        </v-img>
+    </v-card>
+</template>
+
+<script>
+export default {
+    props: [
+        "image",
+        "category"
+    ]
+}
+</script>
---
+++ components/Home.vue
@@ -0,0 +1,32 @@
+<template>
+    <v-container>
+        <v-row justify="center">
+            <v-col cols="6" v-for="item in items" :key="item.category">
+                <Cards :category="item.category" :image="item.image"></Cards>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script>
+import Cards from "./Cards";
+
+export default {
+    components: {
+        Cards
+    },
+    data() {
+        return {
+            items: [
+                { category: "math", image: "https://images.unsplash.com/photo-1635070041078-e363dbe005cb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" },
+                { category: "strings", image: "https://images.unsplash.com/photo-1592431913823-7af6b323da9b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" },
+                { category: "graph", image: "https://images.unsplash.com/photo-1635070041078-e363dbe005cb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" },
+                { category: "search", image: "https://images.unsplash.com/photo-1635070041078-e363dbe005cb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" },
+                { category: "set", image: "https://images.unsplash.com/photo-1635070041078-e363dbe005cb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" },
+                { category: "flow", image: "https://images.unsplash.com/photo-1635070041078-e363dbe005cb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" },
+                { category: "others", image: "https://images.unsplash.com/photo-1635070041078-e363dbe005cb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" }
+            ]
+        }
+    }
+}
+</script>
